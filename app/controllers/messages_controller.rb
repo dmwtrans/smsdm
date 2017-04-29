6d903b9559a32bafacd2806496a5960958497e37
dmwtrans <it@dmwtrans.com>
2017-04-29 16:14:42 -0400

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,6 @@
 class MessagesController < ApplicationController
   include ApplicationHelper
 
-  def index
-    @messages = Message.recent_by_number.by_date
-  end
-
-
 
   def create
     message = Message.new(clean_params)
