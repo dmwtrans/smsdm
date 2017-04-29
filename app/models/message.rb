a6f4d07bc60a2d70b0776a347fbb521f0915eb39
dmwtrans <it@dmwtrans.com>
2017-04-29 16:28:21 -0400

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   scope :for_number,        -> (number) { where(number: number).by_date }
   scope :by_date,           -> { order(created_at: :desc) }
-  scope :recent_by_number,  -> { group(:number).having('created_at = MAX(created_at)') }
+  scope :recent_by_number,  -> { select(:number).having('created_at = MAX(created_at)') }
 
   validates :text,
     presence: true,
