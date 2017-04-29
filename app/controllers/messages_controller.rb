f5af9f29af4c20cc3c97ab02258eb6bcbd9f5641
dmwtrans <it@dmwtrans.com>
2017-04-29 16:15:41 -0400

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,15 @@
 class MessagesController < ApplicationController
   include ApplicationHelper
 
+  def index
+    @messages = Message.recent_by_number.by_date
+  end
 
+  def show
+    @messages = Message.for_number(params[:id])
+    @new_message = Message.new(number: params[:id])
+  end
+  
   def create
     message = Message.new(clean_params)
     message.inbound = false
